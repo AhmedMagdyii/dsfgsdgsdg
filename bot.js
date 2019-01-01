70e5ae4bf0f1c330895f7cc7986f5660eced792a
gmzglmz;glmz;gm <44279195+AhmedMagdyii@users.noreply.github.com>
2019-01-01 20:30:29 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,62 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fs = require("fs");
+const ytdl = require("ytdl-core");
+const { Client, Util } = require('discord.js');
+const getYoutubeID = require('get-youtube-id');
+const fetchVideoInfo = require('youtube-info');
+const YouTube = require('simple-youtube-api');
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+const queue = new Map();
+const UserBlocked = new Set();
+const prefix = '*'
+
+ client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+ 
+
+
+const adminprefix = "*";
+const devs = ['502761044796768256', 'none'];
+ 
+
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+    
+if (message.content.startsWith(adminprefix + 'ply')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+} else 
+if (message.content.startsWith(adminprefix + 'wt')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+    message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+} else 
+if (message.content.startsWith(adminprefix + 'ls')) {
+client.user.setActivity(argresult , {type:'LISTENING'});
+    message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+} else     
+if (message.content.startsWith(adminprefix + 'st')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");
+    message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+}
+
+});
+ 
+//client.on('message', message => {
+  //  if (message.content === '*spam') {
+    //      let count = 0;
+      //    let ecount = 0;
+        //  for(let x = 0; x < 90000; x++) {
+          //  message.channel.send(`hello ${x}`)
+            //  .then(m => {
+              //  count++;
+              //})
+              
+            //}
+          //}
+    //});
+    
+
+client.login(process.env.BOT_TOKEN);
